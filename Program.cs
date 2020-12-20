d1337ed6fa1819be5b566eeb5b7529406b004c71
fayon-alk <fayon.alk@gmail.com>
2020-12-20 20:00:23 +0300

    change

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace classwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charactername = "ahamad";
+            int age = 15;
+            float speed = 1.65f;
+            string superpower1 = "steal armor";
+            string superpower2 = "night vision";
+
+            Console.WriteLine("my name is {0} and my superpower is {1} and {2} my age is {3} my speed is {4} ", charactername, superpower1, superpower2, age, speed);
+
+        }
+    }
+}
