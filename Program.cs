83f8a694868a51394df3f421c2571f66200db70d
fayon-alk <fayon.alk@gmail.com>
2020-12-21 17:53:08 +0300

    second task

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace classwork1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
             string charactername = "ahamad";
             int age = 15;
@@ -13,6 +13,16 @@ namespace classwork1
             string superpower2 = "night vision";
 
             Console.WriteLine("my name is {0} and my superpower is {1} and {2} my age is {3} my speed is {4} ", charactername, superpower1, superpower2, age, speed);
+
+            // second task 
+            string heroname = "mario";
+                string heroheight = "180cm";
+            int heroage = 20;
+            string herosuperpower1 = "fire";
+            string herosuperpower2 = "fast";
+            int agedifference = heroage - age;
+            Console.WriteLine("the age difference between two heroes is {0}", agedifference);
+
 
         }
     }
